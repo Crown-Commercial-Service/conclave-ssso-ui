122f4c9d589e7607df41e744fa0c4526e0a14a9d
Ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2021-10-15 12:50:14 +0100

    github actions

---
+++ src/environments/environment.ts
@@ -0,0 +1,37 @@
+export const environment = {
+    production: true,
+    idam_client_id: 'TPmOI4f0eDrYqzRu2a4BXNhe4vHJt4jY',
+    uri: {
+      api: {
+        isApiGateWayEnabled: false,
+        security: 'https://uat-api-security.london.cloudapps.digital',
+        postgres: 'https://uat-api-core.london.cloudapps.digital',
+        cii: 'https://conclave-cii-integration-brash-shark-mk.london.cloudapps.digital',
+        wrapper: {
+          apiGatewayEnabled: {
+            user: 'https://uat-api-wrapper.london.cloudapps.digital/user-profiles',
+            organisation: 'https://uat-api-wrapper.london.cloudapps.digital/organisation-profiles',
+            contact: 'https://uat-api-wrapper.london.cloudapps.digital/contacts',
+            configuration: 'https://uat-api-wrapper.london.cloudapps.digital/configurations',
+          },
+          apiGatewayDisabled: {
+            user: 'https://uat-api-wrapper.london.cloudapps.digital/users',
+            organisation: 'https://uat-api-wrapper.london.cloudapps.digital/organisations',
+            contact: 'https://uat-api-wrapper.london.cloudapps.digital/contacts',
+            configuration: 'https://uat-api-wrapper.london.cloudapps.digital/configurations',
+          }
+        }
+      },
+      web: {
+        dashboard: 'https://uat-ccs-sso.london.cloudapps.digital'
+      },
+      ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
+    },
+  
+    usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed,
+    listPageSize: 10,
+    rollbar: {
+      key: '39bb38f087ee4a6781b9d76a73200dd8',
+      enable: true
+    }
+  };
