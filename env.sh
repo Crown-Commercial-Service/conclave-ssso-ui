a8fbc60bfa32c632d8a00d0c311e3478a7da1366
Ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2021-10-16 09:32:21 +0100

    travis changes

--- env.sh
+++ env.sh
@@ -5,38 +5,33 @@ if [ "$TRAVIS_BRANCH" = "uat" ]
 then
     IDAM=$IDAM_ID
     ROLLBAR=$ROLLBAR
-    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-uat.ts > src/environments/environment.ts
-    sed "s/ROLLBAR/$ROLLBAR/g" src/environments/environment-uat.ts > src/environments/environment.ts
+    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-uat.ts | sed "s/ROLLBAR/$ROLLBAR/g" > src/environments/environment.ts
 fi
 
 if [ "$TRAVIS_BRANCH" = "training" ]
 then
     IDAM=$IDAM_ID
     ROLLBAR=$ROLLBAR
-    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-training.ts > src/environments/environment.ts
-    sed "s/ROLLBAR/$ROLLBAR/g" src/environments/environment-training.ts > src/environments/environment.ts
+    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-training.ts | sed "s/ROLLBAR/$ROLLBAR/g" > src/environments/environment.ts
 fi
 
 if [ "$TRAVIS_BRANCH" = "pre-production" ]
 then
     IDAM=$IDAM_ID
     ROLLBAR=$ROLLBAR
-    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-pre-production.ts > src/environments/environment.ts
-    sed "s/ROLLBAR/$ROLLBAR/g" src/environments/environment-pre-production.ts > src/environments/environment.ts
+    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-pre-production.ts | sed "s/ROLLBAR/$ROLLBAR/g" > src/environments/environment.ts
 fi
 
 if [ "$TRAVIS_BRANCH" = "sandbox" ]
 then
     IDAM=$IDAM_ID
     ROLLBAR=$ROLLBAR
-    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-sandbox.ts > src/environments/environment.ts
-    sed "s/ROLLBAR/$ROLLBAR/g" src/environments/environment-sandbox.ts > src/environments/environment.ts
+    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-sandbox.ts | sed "s/ROLLBAR/$ROLLBAR/g" > src/environments/environment.ts
 fi
 
 if [ "$TRAVIS_BRANCH" = "testing" ]
 then
     IDAM=$IDAM_ID
     ROLLBAR=$ROLLBAR
-    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-testing.ts > src/environments/environment.ts
-    sed "s/ROLLBAR/$ROLLBAR/g" src/environments/environment-testing.ts > src/environments/environment.ts
+    sed "s/IDAM_ID/$IDAM_ID/g" src/environments/environment-testing.ts | sed "s/ROLLBAR/$ROLLBAR/g" > src/environments/environment.ts
 fi
