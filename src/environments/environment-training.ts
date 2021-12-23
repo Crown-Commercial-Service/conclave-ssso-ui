f360c699b67e5389c7fb36029440fc212b676740
Ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2021-12-23 11:36:56 +0000

    env changes & nginx conf

--- src/environments/environment-training.ts
+++ src/environments/environment-training.ts
@@ -1,73 +1,77 @@
 export const environment = {
 
-    production: true,
-  
-    idam_client_id: 'IDAM_ID',
-  
-    uri: {
-  
-      api: {
-  
-        isApiGateWayEnabled: false,
-  
-        security: 'https://training-api-security.london.cloudapps.digital',
-  
-        postgres: 'https://training-api-core.london.cloudapps.digital',
-  
-        cii: 'https://conclave-cii-integration-brash-shark-mk.london.cloudapps.digital',
-  
-        wrapper: {
-  
-          apiGatewayEnabled: {
-  
-            user: 'https://training-api-wrapper.london.cloudapps.digital/user-profiles',
-  
-            organisation: 'https://training-api-wrapper.london.cloudapps.digital/organisation-profiles',
-  
-            contact: 'https://training-api-wrapper.london.cloudapps.digital/contacts',
-  
-            configuration: 'https://training-api-wrapper.london.cloudapps.digital/configurations',
-  
-          },
-  
-          apiGatewayDisabled: {
-  
-            user: 'https://training-api-wrapper.london.cloudapps.digital/users',
-  
-            organisation: 'https://training-api-wrapper.london.cloudapps.digital/organisations',
-  
-            contact: 'https://training-api-wrapper.london.cloudapps.digital/contacts',
-  
-            configuration: 'https://training-api-wrapper.london.cloudapps.digital/configurations',
-  
-          }
-  
-        }
-  
-      },
-  
-      web: {
-  
-        dashboard: 'https://training-ccs-sso.london.cloudapps.digital'
-  
-      },
-  
-      ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
-  
-    },
-  
-    googleTagMangerId: 'GTM',
+  production: true,
 
-    usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed, 
+  idam_client_id: 'IDAM_ID',
+
+  uri: {
+
+    api: {
+
+      isApiGateWayEnabled: false,
+
+      security: 'https://training-api-security.london.cloudapps.digital',
+
+      postgres: 'https://training-api-core.london.cloudapps.digital',
+
+      cii: 'https://conclave-cii-integration-brash-shark-mk.london.cloudapps.digital',
+
+      wrapper: {
+
+        apiGatewayEnabled: {
+
+          user: 'https://training-api-wrapper.london.cloudapps.digital/user-profiles',
+
+          organisation: 'https://training-api-wrapper.london.cloudapps.digital/organisation-profiles',
+
+          contact: 'https://training-api-wrapper.london.cloudapps.digital/contacts',
+
+          configuration: 'https://training-api-wrapper.london.cloudapps.digital/configurations',
+
+        },
+
+        apiGatewayDisabled: {
+
+          user: 'https://training-api-wrapper.london.cloudapps.digital/users',
+
+          organisation: 'https://training-api-wrapper.london.cloudapps.digital/organisations',
+
+          contact: 'https://training-api-wrapper.london.cloudapps.digital/contacts',
+
+          configuration: 'https://training-api-wrapper.london.cloudapps.digital/configurations',
+
+        }
+
+      }
+
+    },
+
+    web: {
+
+      dashboard: 'https://training-ccs-sso.london.cloudapps.digital'
+
+    },
+
+    ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
+
+  },
+
+  googleTagMangerId: 'GTM',
   
-    listPageSize: 10,
+  bulkUploadMaxFileSizeInBytes:1048576,
   
-    rollbar: {
+  bulkUploadTemplateFileUrl: 'https://paas-s3-broker-prod-lon-78ff9dde-5c67-4eb6-82f8-c9c1fbadb459.s3.amazonaws.com/Templates/DataMigrationTemplate.csv',
   
-      key: 'ROLLBAR',
-  
-      enable: true
-  
-    }
-  
-  };
+  usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed, 
+
+  listPageSize: 10,
+
+  rollbar: {
+
+    key: 'ROLLBAR',
+
+    enable: true
+
+  }
+
+};
