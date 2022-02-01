eeaf6b1e53919d0a35331519748179fa64440bf3
Tharindu Subashith <tharindu.s@geveo.com>
2022-02-01 15:40:14 +0530

    user copntact area

--- src/app/constants/constant.ts
+++ src/app/constants/constant.ts
@@ -1,4 +1,5 @@
 export const SessionStorageKey = {
     ManageUserUserName : "manage_user_username",
-    OperationSuccessUserName : "operation_success_username"
+    OperationSuccessUserName : "operation_success_username",
+    UserContactUsername: "user_contact_user_name"
 }
