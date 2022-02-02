fec767082b42dc4257439948c7999a7142dbf7fb
tsbrickendon <84302916+tsbrickendon@users.noreply.github.com>
2022-02-02 11:52:03 +0530

    Remove username form url for GTM (#180)
    * Change manage user area email urls
    
    * user copntact area
    
    * mfa related area
    
    * org user support area
    
    Co-authored-by: Tharindu Subashith <tharindu.s@geveo.com>

---
+++ src/app/constants/constant.ts
@@ -0,0 +1,7 @@
+export const SessionStorageKey = {
+    ManageUserUserName : "manage_user_username",
+    OperationSuccessUserName : "operation_success_username",
+    UserContactUsername: "user_contact_user_name",
+    MFAResetUserName: "mfa_reset_user_name",
+    OrgUserSupportUserName: "org_user_support_user_name"
+}
