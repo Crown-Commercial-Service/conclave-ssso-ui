892b155d14681eb26d430cb7deb03f325459867a
Ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2022-02-03 05:20:17 +0000

    env files fixed

---
+++ src/environments/environment-pre-production.ts
@@ -0,0 +1,84 @@
+export const environment = {
+
+    production: false,
+  
+    idam_client_id: 'IDAM',
+  
+    uri: {
+  
+        api: {
+  
+            isApiGateWayEnabled: true,
+  
+            security: 'https://pre.api.crowncommercial.gov.uk',
+  
+            //security: 'https://pre.api.crowncommercial.gov.uk', 
+  
+            postgres: 'https://pre.api.crowncommercial.gov.uk/core',
+  
+            //postgres: 'https://pre.api.crowncommercial.gov.uk/core', 
+  
+            wrapper: {
+  
+                apiGatewayEnabled: {
+  
+                    user: 'https://pre.api.crowncommercial.gov.uk/user-profiles',
+  
+                    organisation: 'https://pre.api.crowncommercial.gov.uk/organisation-profiles',
+  
+                    contact: 'https://pre.api.crowncommercial.gov.uk/contacts',
+  
+                    configuration: 'https://pre.api.crowncommercial.gov.uk/configurations',
+  
+                },
+  
+                apiGatewayDisabled: {
+  
+                    user: 'https://preprod-api-wrapper.london.cloudapps.digital/users',
+  
+                    organisation: 'https://preprod-api-wrapper.london.cloudapps.digital/organisations',
+  
+                    contact: 'https://preprod-api-wrapper.london.cloudapps.digital/contacts',
+  
+                    configuration: 'https://preprod-api-wrapper.london.cloudapps.digital/configurations',
+  
+                }
+  
+            }
+  
+        },
+  
+        web: {
+  
+            //dashboard: 'https://preprod-ccs-sso.london.cloudapps.digital' 
+  
+            dashboard: 'https://preprod.identify.crowncommercial.gov.uk'
+  
+        },
+  
+        ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
+  
+    },
+  
+    googleTagMangerId: 'GTM',
+  
+    bulkUploadPollingFrequencyInSeconds: 5,
+  
+    bulkUploadMaxFileSizeInBytes:1048576,
+  
+    bulkUploadTemplateFileUrl: 'https://paas-s3-broker-prod-lon-9c4ee920-fba7-41df-989f-495ed39c9ade.s3.amazonaws.com/Templates/DataMigrationTemplate.csv',
+  
+    usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed, 
+  
+    listPageSize: 10,
+  
+    rollbar: {
+  
+        key: 'ROLLBAR',
+  
+        enable: true
+  
+    }
+  
+  };
+  
