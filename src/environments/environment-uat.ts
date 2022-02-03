feb5de3102966c74eeb6d4b3cc4146843412de19
Ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2022-02-03 05:20:17 +0000

    env files fixed

---
+++ src/environments/environment-uat.ts
@@ -0,0 +1,46 @@
+export const environment = {
+    production: true,
+    idam_client_id: 'IDAM',
+    uri: {
+      api: {
+        isApiGateWayEnabled: true,
+        //security: 'https://uat-api-security.london.cloudapps.digital',
+        security: 'https://ref.api.crowncommercial.gov.uk',
+        //postgres: 'https://uat-api-core.london.cloudapps.digital',
+        postgres: 'https://ref.api.crowncommercial.gov.uk/core',
+        //cii: 'https://conclave-cii-integration-brash-shark-mk.london.cloudapps.digital',
+        wrapper: {
+          apiGatewayEnabled: {
+            user: 'https://ref.api.crowncommercial.gov.uk/user-profiles',
+            organisation: 'https://ref.api.crowncommercial.gov.uk/organisation-profiles',
+            contact: 'https://ref.api.crowncommercial.gov.uk/contacts',
+            configuration: 'https://ref.api.crowncommercial.gov.uk/configurations',
+          },
+          apiGatewayDisabled: {
+            user: 'https://uat-api-wrapper.london.cloudapps.digital/users',
+            organisation: 'https://uat-api-wrapper.london.cloudapps.digital/organisations',
+            contact: 'https://uat-api-wrapper.london.cloudapps.digital/contacts',
+            configuration: 'https://uat-api-wrapper.london.cloudapps.digital/configurations',
+          }
+        }
+      },
+      web: {
+        //dashboard: 'https://uat-ccs-sso.london.cloudapps.digital'
+        dashboard: 'https://ref.identify.crowncommercial.gov.uk'
+      },
+      ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
+    },
+  
+    googleTagMangerId: 'GTM',
+    bulkUploadPollingFrequencyInSeconds: 5,
+    bulkUploadMaxFileSizeInBytes:1048576,
+    bulkUploadTemplateFileUrl: 'https://paas-s3-broker-prod-lon-55773e8e-fa69-4bee-ab0d-25f66171b4d0.s3.amazonaws.com/Templates/DataMigrationTemplate.csv',
+    usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed,
+    listPageSize: 10,
+    rollbar: {
+      key: 'ROLLBAR',
+      enable: true
+    }
+  };
+  
+  
