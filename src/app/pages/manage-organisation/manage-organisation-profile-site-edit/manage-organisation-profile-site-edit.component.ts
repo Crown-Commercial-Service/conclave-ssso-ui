245f8313da6fee36a19d5ff4ad7406e95374867b
Tharindu Subashith <tharindu.s@geveo.com>
2022-02-10 10:33:04 +0530

    Add initial searchable country list

--- src/app/pages/manage-organisation/manage-organisation-profile-site-edit/manage-organisation-profile-site-edit.component.ts
+++ src/app/pages/manage-organisation/manage-organisation-profile-site-edit/manage-organisation-profile-site-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { ViewportScroller } from '@angular/common';
@@ -15,6 +15,9 @@ import { Title } from '@angular/platform-browser';
 import { FormBaseComponent } from 'src/app/components/form-base/form-base.component';
 import { ContryDetails } from 'src/app/models/contryDetails';
 import { WrapperConfigurationService } from 'src/app/services/wrapper/wrapper-configuration.service';
+import { ReplaySubject, Subject } from 'rxjs';
+import { MatSelect } from '@angular/material/select';
+import { take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manage-organisation-profile-site-edit',
@@ -31,8 +34,13 @@ export class ManageOrganisationSiteEditComponent extends FormBaseComponent imple
   contactColumnsToDisplay = ['contactReason', 'name', 'email', 'phoneNumber', 'fax', 'webUrl'];
   contactData: ContactGridInfo[];
   countryDetails: ContryDetails[] = [];
+  filteredCountryDetails: ReplaySubject<ContryDetails[]> = new ReplaySubject<ContryDetails[]>(1);
+
+  public bankFilterCtrl: FormControl = new FormControl();
+  protected _onDestroy = new Subject<void>();
 
   @ViewChildren('input') inputs!: QueryList<ElementRef>;
+  @ViewChild('singleSelect', { static: true }) singleSelect!: MatSelect;
 
   constructor(private formBuilder: FormBuilder, private router: Router, private activatedRoute: ActivatedRoute,
     protected uiStore: Store<UIState>, protected viewportScroller: ViewportScroller, protected scrollHelper: ScrollHelper,
@@ -59,6 +67,15 @@ export class ManageOrganisationSiteEditComponent extends FormBaseComponent imple
   async ngOnInit() {
     this.titleService.setTitle(`${this.isEdit ? "Edit" : "Add"} - Site - CCS`);
     this.countryDetails = await this.wrapperConfigService.getCountryDetails().toPromise();
+    this.filteredCountryDetails.next(this.countryDetails.slice());
+
+    // listen for search field value changes
+    this.bankFilterCtrl.valueChanges
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.filterBanks();
+      });
+
     console.log("CountryDetails", this.countryDetails);
     if (this.isEdit) {
       this.orgSiteService.getOrganisationSite(this.organisationId, this.siteId).subscribe(
@@ -70,7 +87,7 @@ export class ManageOrganisationSiteEditComponent extends FormBaseComponent imple
               locality: siteInfo.address.locality,
               region: siteInfo.address.region,
               postalCode: siteInfo.address.postalCode,
-              countryCode: siteInfo.address.countryCode,
+              countryCode: this.countryDetails.find(s => s.countryCode == siteInfo.address.countryCode),
             });
             this.getSiteContacts();
             this.onFormValueChange();
@@ -83,6 +100,49 @@ export class ManageOrganisationSiteEditComponent extends FormBaseComponent imple
     else {
       this.onFormValueChange();
     }
+  }
+
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
+
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  /**
+   * Sets the initial value after the filteredBanks are loaded initially
+   */
+  protected setInitialValue() {
+    this.filteredCountryDetails
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
+        // the form control (i.e. _initializeSelection())
+        // this needs to be done after the filteredBanks are loaded initially
+        // and after the mat-option elements are available
+        this.singleSelect.compareWith = (a: ContryDetails, b: ContryDetails) => a && b && a.id === b.id;
+      });
+  }
+
+  protected filterBanks() {
+    if (!this.countryDetails) {
+      return;
+    }
+    // get the search keyword
+    let search = this.bankFilterCtrl.value;
+    if (!search) {
+      this.filteredCountryDetails.next(this.countryDetails.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredCountryDetails.next(
+      this.countryDetails.filter(countryDetail => countryDetail.countryName.toLowerCase().indexOf(search) > -1)
+    );
   }
 
   getSiteContacts() {
@@ -121,7 +181,7 @@ export class ManageOrganisationSiteEditComponent extends FormBaseComponent imple
           locality: form.get('locality')?.value,
           region: form.get('region')?.value,
           postalCode: form.get('postalCode')?.value,
-          countryCode: form.get('countryCode')?.value,
+          countryCode: form.get('countryCode')?.value?.countryCode,
         }
       };
 
