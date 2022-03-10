f130dc6c42a90c2d3cde6971bfa11b5d9d4cdd82
Ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2022-03-10 07:42:31 +0000

    travis & env changes

---
+++ src/environments/environment-uat.ts
@@ -0,0 +1,46 @@
+export const environment = {
+    production: true,
+    idam_client_id: 'IDAM_ID',
+    uri: {
+      api: {
+        isApiGateWayEnabled: true,
+        //security: 'https://uat-api-security.london.cloudapps.digital',
+        security: 'https://uat.api.crowncommercial.gov.uk',
+        //postgres: 'https://uat-api-core.london.cloudapps.digital',
+        postgres: 'https://uat.api.crowncommercial.gov.uk/core',
+        //cii: 'https://conclave-cii-integration-brash-shark-mk.london.cloudapps.digital',
+        wrapper: {
+          apiGatewayEnabled: {
+            user: 'https://uat.api.crowncommercial.gov.uk/user-profiles',
+            organisation: 'https://uat.api.crowncommercial.gov.uk/organisation-profiles',
+            contact: 'https://uat.api.crowncommercial.gov.uk/contacts',
+            configuration: 'https://uat.api.crowncommercial.gov.uk/configurations',
+          },
+          apiGatewayDisabled: {
+            user: 'https://uat-api-wrapper.london.cloudapps.digital/users',
+            organisation: 'https://uat-api-wrapper.london.cloudapps.digital/organisations',
+            contact: 'https://uat-api-wrapper.london.cloudapps.digital/contacts',
+            configuration: 'https://uat-api-wrapper.london.cloudapps.digital/configurations',
+          }
+        }
+      },
+      web: {
+        //dashboard: 'https://uat-ccs-sso.london.cloudapps.digital'
+        dashboard: 'https://uat.identify.crowncommercial.gov.uk'
+      },
+      ccsContactUrl: "https://www.crowncommercial.gov.uk/contact"
+    },
+  
+    googleTagMangerId: 'GTM',
+    bulkUploadPollingFrequencyInSeconds: 5,
+    bulkUploadMaxFileSizeInBytes:1048576,
+    bulkUploadTemplateFileUrl: 'BUCKET_URL',
+    usedPasswordThreshold: 5, //This value should be changed when Auth0 password history policy changed,
+    listPageSize: 10,
+    rollbar: {
+      key: 'ROLLBAR',
+      enable: true
+    }
+  };
+  
+  
