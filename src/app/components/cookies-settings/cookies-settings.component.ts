d84ae681c78843d4becf8adbed56349f8eb6b43b
karpagamnallasamy-bc <karpagam.nallasamy@brickendon.com>
2022-03-31 15:06:45 +0530

    Page design completed

--- src/app/components/cookies-settings/cookies-settings.component.ts
+++ src/app/components/cookies-settings/cookies-settings.component.ts
@@ -7,17 +7,22 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./cookies-settings.component.scss']
 })
 export class CookiesSettingsComponent implements OnInit {
-  essentialcookies_Header = ['Name', 'Purpose', 'Expires'];
-  essentialcookies_Display= ['notify_admin_session', 'Used to keep you signed in', '20'];
-  public essentialcookiesData: any = [
-    'notify_admin_session','Used to keep you signed in','20',
-  ];
-
   notify_admin_session =  environment.cookies_policy.essentialcookies.notify_admin_session;
   cookie_policy =  environment.cookies_policy.essentialcookies.cookie_policy;
+  
+  ccs_sso_visitedsites=environment.cookies_policy.essentialcookies['ccs-sso-visitedsites']
+  opbs =  environment.cookies_policy.essentialcookies.opbs;
+  ccs_sso=environment.cookies_policy.essentialcookies['ccs-sso'];
+  conclave=environment.cookies_policy.essentialcookies.conclave;
+  XSRF_TOKEN=environment.cookies_policy.essentialcookies['XSRF-TOKEN'];
+  XSRF_TOKEN_SVR=environment.cookies_policy.essentialcookies['XSRF-TOKEN-SVR'];
+  AspNetCore_Antiforgery_GWNWkbbyKbw= environment.cookies_policy.essentialcookies['AspNetCore.Antiforgery.GWNWkbbyKbw']
 
-  _ga =  environment.cookies_policy.analyticscookies._ga;
-  _gid =  environment.cookies_policy.analyticscookies._gid;
+  auth0_compat =  environment.cookies_policy.Auth0cookies.auth0_compat;
+  did_compat =  environment.cookies_policy.Auth0cookies.did_compat;
+  did =  environment.cookies_policy.Auth0cookies.did;
+  auth0 =  environment.cookies_policy.Auth0cookies.auth0;
+  _cf_bm =  environment.cookies_policy.Auth0cookies.__cf_bm;
 
   constructor() { }
 
