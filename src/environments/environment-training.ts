75b0b66b4401df67f5506237eb27dd0c92f3ef3f
Ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2022-04-06 10:00:38 +0100

    env changes

--- src/environments/environment-training.ts
+++ src/environments/environment-training.ts
@@ -57,6 +57,8 @@ export const environment = {
   },
 
   googleTagMangerId: 'GTM',
+
+  cookieExpirationTimeInMinutes: 525600,
   
   bulkUploadPollingFrequencyInSeconds: 5,
   
@@ -74,6 +76,26 @@ export const environment = {
 
     enable: true
 
+  },
+  cookies_policy: {
+    essentialcookies: {
+        notify_admin_session: 20,
+        cookie_policy: 1,
+        ccs_sso_visitedsites: 60,
+        opbs: 60,
+        ccs_sso: 60,
+        conclave: 60,
+        XSRF_TOKEN: 30,
+        XSRF_TOKEN_SVR: 30,
+        AspNetCore_Antiforgery_GWNWkbbyKbw: 30
+    },
+    Auth0cookies: {
+        auth0_compat: 3,
+        did_compat: 6,
+        did: 6,
+        auth0: 3,
+        __cf_bm: 30
+    },
   }
 
 };
