9719398fbb6ff58419fe101272d7b73b9f89a47e
ajithmuthukumar-bc <99731656+ajithmuthukumar-bc@users.noreply.github.com>
2022-05-09 20:08:54 +0530

    updated (#482)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  idam_client_id: '5G2pNShwaBQzjJNx6fCSqt9ZIAU9sEur',
+  idam_client_id: '',
   uri: {
     api: {
       isApiGateWayEnabled: false,
@@ -35,7 +35,7 @@ export const environment = {
   listPageSize: 10,
   rollbar: {
     key: '',
-    enable: false,
+    enable: true,
     security_log:true
   } ,
   cookies_policy: {
