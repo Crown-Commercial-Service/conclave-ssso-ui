6fffddcb5cedc6b9591f5f53bac948636068b7c6
ajithmuthukumar-bc <99731656+ajithmuthukumar-bc@users.noreply.github.com>
2022-06-14 20:32:00 +0530

    P2sprint9 (#558)
    * mfa style changes (#556)
    
    * content changed (#557)

--- src/app/pages/accessibility-statement/accessibility-statement.component.ts
+++ src/app/pages/accessibility-statement/accessibility-statement.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccessibilityStatementComponent implements OnInit {
 
+public representingTag={ 
+  LevelA:'<span _ngcontent-ary-c81="" style="font-weight: 600; font-size: 24px;">',
+  LevelAA:'<h1>',
+  KeyboardLevelA:'<a>',
+  PageTitledLevelA:'<html>'
+}
+
   constructor() { }
 
   ngOnInit(): void {
