aceedf418bf9d425e08a5991d2dab17932833e84
ajithmuthukumar-bc <ajith.muthukumar@brickendon.com>
2022-08-23 10:02:25 +0530

    #

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -249,6 +249,7 @@ export class AuthService {
     localStorage.removeItem('cii_organisation_id');
     localStorage.removeItem('at_exp');
     localStorage.removeItem('permission_organisation_id');
+    localStorage.removeItem('delegatedOrg');
   }
 
   public logOutAndRedirect() {
