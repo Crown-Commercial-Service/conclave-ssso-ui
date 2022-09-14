3ed9ed003908c4bbd1852c0ea5797853d8c52f4e
VijayHirudayasamy-bc <vijay.hirudayasamy@brickendon.com>
2022-09-14 15:05:24 +0100

    Auth0 Rules are updated in the frontend project

--- Auth0Rules/4_MFABasedOnCustomAttribute.js
+++ Auth0Rules/4_MFABasedOnCustomAttribute.js
@@ -1,6 +1,9 @@
 function guardianMultifactor(user, context, callback) {
     //const CLIENTS_WITH_MFA = ['REPLACE_WITH_YOUR_CLIENT_ID'];
 
+    //console.log("MFA log 1");
+    //console.log(user);
+
     if (context.protocol === 'oauth2-refresh-token')
         return callback(null, user, context);
 
@@ -8,27 +11,38 @@ function guardianMultifactor(user, context, callback) {
     let authMethods = [];
     if (context.authentication && Array.isArray(context.authentication.methods)) {
         authMethods = context.authentication.methods;
+        console.log("MFA log2  context.authentication.methods");
+        console.log(context.authentication.methods);
     }
 
-    const completedMfa = !!authMethods.find((method) => method.name === 'mfa');
+    let findMFA = authMethods.find((method) => method.name === 'mfa');
+    console.log("Finding authMethods for mfa", findMFA);
+    console.log("Finding authMethods for !!mfa", !!findMFA);
 
+
+    const completedMfa = !!authMethods.find((method) => method.name === 'mfa');
+    console.log("MFA log3  ccompletedMfa");
+    console.log(completedMfa);
     if (completedMfa) {
         return callback(null, user, context);
     }
 
+    let socialLogin = !!user.identities.find((identity) => identity.provider === "google-oauth2");
+    console.log('user.user_metadata-', user.user_metadata);
 
     // run only for the specified clients
     //if (CLIENTS_WITH_MFA.indexOf(context.clientID) !== -1) {
 
     // uncomment the following if clause in case you want to request a second factor only from user's that have user_metadata.use_mfa === true
     if (user.user_metadata && user.user_metadata.use_mfa === true) {
+        console.log('Inside mfa true condition');
         context.multifactor = {
             // required
             provider: 'any',
 
             // optional, defaults to true. Set to false to force Guardian authentication every time.
             // See https://auth0.com/docs/multifactor-authentication/custom#change-the-frequency-of-authentication-requests for details
-            allowRememberBrowser: true
+            allowRememberBrowser: false
         };
         //}
     }
