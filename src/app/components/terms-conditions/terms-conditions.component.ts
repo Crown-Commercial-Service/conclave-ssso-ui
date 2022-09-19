4a7faf8a01654d0301aabb4e327719ca9039da02
ajithmuthukumar-bc <99731656+ajithmuthukumar-bc@users.noreply.github.com>
2022-09-19 18:01:16 +0530

    Delegated bug sprint14 (#737)
    * #
    
    * #
    
    * #
    
    * #
    
    * #

---
+++ src/app/components/terms-conditions/terms-conditions.component.ts
@@ -0,0 +1,32 @@
+import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-terms-conditions',
+  templateUrl: './terms-conditions.component.html',
+  styleUrls: ['./terms-conditions.component.scss']
+})
+export class TermsConditionsComponent implements OnInit {
+  private userName = localStorage.getItem('user_name') || '';
+  constructor(private router: Router,private scroller: ViewportScroller) { }
+
+  ngOnInit(): void {
+  }
+
+ 
+  public navigateToHome():void{
+    if(this.userName){
+      this.router.navigateByUrl(`home`);
+    }else{
+      window.location.href='https://www.crowncommercial.gov.uk';  
+    }
+  }
+
+  public scrollContent(id:string):void{
+    document.getElementById(id) ?.scrollIntoView({
+      block: "start",
+      inline: "nearest"
+    });
+  }
+}
