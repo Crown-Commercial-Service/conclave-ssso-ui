3afee0300b2670606b6a1d1871d0c008bac708cf
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2022-09-26 12:51:26 +0530

    Update environment-development.ts

--- src/environments/environment-development.ts
+++ src/environments/environment-development.ts
@@ -40,7 +40,7 @@ export const environment = {
     rollbar: {
         key: 'ROLLBAR',
         enable: true,
-	security_log: true,
+	security_log: false,
       environment: 'development'
     },
     cookies_policy: {
