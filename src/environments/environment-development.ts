461703940414c9504d7ba666f517cd65e66a4720
ajithmuthukumar-bc <99731656+ajithmuthukumar-bc@users.noreply.github.com>
2022-09-27 10:22:31 +0530

    Update environment-development.ts

--- src/environments/environment-development.ts
+++ src/environments/environment-development.ts
@@ -41,7 +41,7 @@ export const environment = {
         key: 'ROLLBAR',
         enable: true,
         security_log: false,
-        environment: 'development'
+        environment: 'QA Testing'
     },
     cookies_policy: {
         essentialcookies: {
