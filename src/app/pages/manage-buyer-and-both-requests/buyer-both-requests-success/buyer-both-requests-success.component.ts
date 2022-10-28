53ed9caee21b6cfd1e2e59ded91f3fbb2b89e651
ajithmuthukumar-bc <ajith.muthukumar@brickendon.com>
2022-10-28 14:30:51 +0530

    changes are updated

--- src/app/pages/manage-buyer-and-both-requests/buyer-both-requests-success/buyer-both-requests-success.component.ts
+++ src/app/pages/manage-buyer-and-both-requests/buyer-both-requests-success/buyer-both-requests-success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -8,9 +9,28 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class BuyerBothRequestsSuccessComponent implements OnInit {
   public routeDetails:any;
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(private route: ActivatedRoute, private router: Router, private titleService: Title) {
     this.route.queryParams.subscribe((para: any) => {
       this.routeDetails = JSON.parse(atob(para.data));
+      console.log("this.routeDetails",this.routeDetails)
+      switch (this.routeDetails.status) {
+        case "accept": {
+          this.titleService.setTitle(
+            `${ 'Accept right to buy status – success' }  - CCS`
+          );
+          break;
+        }
+        case 'decline': {
+          this.titleService.setTitle(
+            `${ 'Decline right to buy status – success' }  - CCS`
+          );
+          break;
+        }
+        default: {
+          //statements;
+          break;
+        }
+      }
     });
   }
 
