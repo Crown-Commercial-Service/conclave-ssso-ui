9e0e6267da8bea891c5091bebd0dfb87178a2e9a
ajithmuthukumar-bc <ajith.muthukumar@brickendon.com>
2023-01-13 23:10:11 +0530

    4394 issue fixed

--- src/app/pages/manage-role/manage-user-role/manage-user-role.component.ts
+++ src/app/pages/manage-role/manage-user-role/manage-user-role.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { acceptRejectRequestDetail } from 'src/app/models/user';
 import { WrapperUserService } from 'src/app/services/wrapper/wrapper-user.service';
 
 @Component({
@@ -11,7 +10,10 @@ import { WrapperUserService } from 'src/app/services/wrapper/wrapper-user.servic
 export class ManageUserRoleComponent implements OnInit {
 public userDetails:any
 public errorResponce:boolean = false;
-  constructor(private wrapperUserService: WrapperUserService,private router: Router,private route: ActivatedRoute) { }
+public isOrgAdmin: boolean = false;
+  constructor(private wrapperUserService: WrapperUserService,private router: Router,private route: ActivatedRoute) { 
+    this.isOrgAdmin = JSON.parse(localStorage.getItem('isOrgAdmin') || 'false');
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((para: any) => {
