aef2463513e0a9e735e79dbe9ca2a354f7a6a9d3
ajithmuthukumar-bc <ajith.muthukumar@brickendon.com>
2023-01-20 00:26:06 +0530

    dublication check

--- src/app/pages/manage-buyer-and-both-requests/view-verified-org/view-verified-org.component.ts
+++ src/app/pages/manage-buyer-and-both-requests/view-verified-org/view-verified-org.component.ts
@@ -287,14 +287,7 @@ export class ViewVerifiedOrgComponent implements OnInit {
       next: async (orgListResponse: OrganisationAuditListResponse) => {
         this.getSchemeData()
         if (orgListResponse != null && orgListResponse.organisationAuditList.length != 0) {
-            let orgDetails:any = orgListResponse.organisationAuditList.find((element) => element.organisationId === this.routeDetails.event.organisationId)
-            if (orgDetails != undefined) {
-              orgDetails.lastRoute ="view-verified"
-              this.router.navigateByUrl(
-                'pending-verification?data=' + btoa(JSON.stringify(orgDetails))
-              );
-            } 
-            
+          this.verficatingOrgnisation(orgListResponse)
         }
       },
       error: (error: any) => {
@@ -302,6 +295,17 @@ export class ViewVerifiedOrgComponent implements OnInit {
       },
     });
   }
+
+  private verficatingOrgnisation(orgListResponse: OrganisationAuditListResponse){
+      let orgDetails:any = orgListResponse.organisationAuditList.find((element) => element.organisationId === this.routeDetails.event.organisationId)
+      if (orgDetails != undefined) {
+        orgDetails.lastRoute ="view-verified"
+        this.router.navigateByUrl(
+          'pending-verification?data=' + btoa(JSON.stringify(orgDetails))
+        );
+      } 
+  }
+  
 
   getVerifiedOrg() {
     this.wrapperBuyerAndBothService.getVerifiedOrg(
