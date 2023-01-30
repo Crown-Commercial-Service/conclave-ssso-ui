04bf4d2a7562a04dcfa2c9c8fd915d6fb3b61110
ajithmuthukumar-bc <99731656+ajithmuthukumar-bc@users.noreply.github.com>
2023-01-30 13:30:43 +0530

    4509 issue fixes (#1071)

--- src/app/pages/manage-user/manage-user-add-single-user-detail/manage-user-add-single-user-detail.component.ts
+++ src/app/pages/manage-user/manage-user-add-single-user-detail/manage-user-add-single-user-detail.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   ElementRef,
+  OnDestroy,
   OnInit,
   QueryList,
   ViewChildren,
@@ -29,6 +30,8 @@ import { PatternService } from 'src/app/shared/pattern.service';
 import { WrapperConfigurationService } from 'src/app/services/wrapper/wrapper-configuration.service';
 import { environment } from 'src/environments/environment';
 import { WrapperOrganisationService } from 'src/app/services/wrapper/wrapper-org-service';
+import { SharedDataService } from 'src/app/shared/shared-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-manage-user-add-single-user-detail',
@@ -37,7 +40,7 @@ import { WrapperOrganisationService } from 'src/app/services/wrapper/wrapper-org
 })
 export class ManageUserAddSingleUserDetailComponent
   extends FormBaseComponent
-  implements OnInit {
+  implements OnInit,OnDestroy {
   organisationId: string;
   userProfileRequestInfo: UserProfileRequestInfo;
   userProfileResponseInfo: UserProfileResponseInfo;
@@ -73,7 +76,7 @@ export class ManageUserAddSingleUserDetailComponent
   private selectedRoleIds: number[] = []
   @ViewChildren('input') inputs!: QueryList<ElementRef>;
   isInvalidDomain: boolean = false
-
+  subscription: Subscription = new Subscription;
   constructor(
     private organisationGroupService: WrapperOrganisationGroupService,
     private configWrapperService: WrapperConfigurationService,
@@ -88,7 +91,8 @@ export class ManageUserAddSingleUserDetailComponent
     private wrapperUserService: WrapperUserService,
     private authService: AuthService,
     private locationStrategy: LocationStrategy,
-    private organisationService: WrapperOrganisationService
+    private organisationService: WrapperOrganisationService,
+    private sharedDataService:SharedDataService
   ) {
     super(
       viewportScroller,
@@ -124,15 +128,13 @@ export class ManageUserAddSingleUserDetailComponent
     this.state = this.router.getCurrentNavigation()?.extras.state;
     this.ciiOrganisationId = localStorage.getItem('cii_organisation_id') || '';
     localStorage.removeItem('user_approved_role');
-    // this.locationStrategy.onPopState(() => {
-    //   this.onCancelClick();
-    // });
     if (queryParams.data) {
-      this.routeData = JSON.parse(atob(queryParams.data));
-      this.isEdit = this.routeData['isEdit'];
-      this.editingUserName =
-        sessionStorage.getItem(SessionStorageKey.ManageUserUserName) ?? '';
-      this.editingUserName = this.routeData.rowData;
+      this.subscription = this.sharedDataService.userEditDetails.subscribe((data)=>{
+        this.routeData = JSON.parse(atob(queryParams.data));
+        this.isEdit = this.routeData['isEdit'];
+        this.editingUserName = sessionStorage.getItem(SessionStorageKey.ManageUserUserName) ?? '';
+        this.editingUserName = data.rowData;
+      })
     }
     this.orgGroups = [];
     this.orgRoles = [];
@@ -832,5 +834,9 @@ export class ManageUserAddSingleUserDetailComponent
         'confirm-user-mfa-reset?data=' + btoa(JSON.stringify(data))
       );
     }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 }
