a1d7601c421ad9cbd786822276e57f0e57dccd4d
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2023-02-23 15:54:53 +0530

    Update environment-NFT.ts

--- src/environments/environment-NFT.ts
+++ src/environments/environment-NFT.ts
@@ -104,9 +104,9 @@ export const environment = {
     },
   },
   appSetting: {
-   hideIDP:false,
+   hideIDP:true,
    hideDelegation:false,
-   hideBulkupload:false,
+   hideBulkupload:true,
    hideAutoValidation:false,
   },
 
