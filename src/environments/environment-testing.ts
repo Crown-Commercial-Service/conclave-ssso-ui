9a472b532c0b7afa9a8f9893a9b506e253cd7600
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2023-05-31 18:16:34 +0530

    Update environment-testing.ts

--- src/environments/environment-testing.ts
+++ src/environments/environment-testing.ts
@@ -13,7 +13,7 @@ export const environment = {
           organisation: 'https://tst.api.crowncommercial.gov.uk/organisation-profiles',
           contact: 'https://tst.api.crowncommercial.gov.uk/contacts',
           configuration: 'https://tst.api.crowncommercial.gov.uk/configurations',
-          dataMigration: 'https://tst.api.crowncommercial.gov.uk/datamigration',
+          dataMigration: 'https://test-api-wrapper.london.cloudapps.digital/datamigration',
         },
         apiGatewayDisabled: {
           user: 'https://test-api-wrapper.london.cloudapps.digital/users',
