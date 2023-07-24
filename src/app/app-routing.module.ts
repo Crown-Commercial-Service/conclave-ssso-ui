25a12b9d0198fe626b49fbe4b346ddeeac323554
ajithmuthukumar-bc <99731656+ajithmuthukumar-bc@users.noreply.github.com>
2023-07-24 12:42:38 +0530

    changes are done (#1528)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -344,7 +344,7 @@ const routes: Routes = [
   },
   {
     path: 'manage-org/register/type',
-    data: { title: 'Buyer/Supplier Type - Registration' },
+    data: { title: 'Organisation type' },
     pathMatch: 'full',
     component: ManageOrgRegRightToBuyComponent,
   },
