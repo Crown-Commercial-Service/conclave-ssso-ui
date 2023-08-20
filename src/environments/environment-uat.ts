3222cd95f190192419e57e85b7e49ca94cbb6bfd
ponselvamsakthivel-bc <ponselvam.sakthivel@brickendon.com>
2023-08-20 22:48:52 +0530

    uat config for p3sprint16

--- src/environments/environment-uat.ts
+++ src/environments/environment-uat.ts
@@ -5,7 +5,7 @@ export const environment = {
     api: {
       isApiGateWayEnabled: false,
       //security: 'https://uat-api-security.london.cloudapps.digital',
-      security: 'https://uat.ppg-sso-service.crowncommercial.gov.uk/security',
+      security: 'https://uat.ppg-sso-service.crowncommercial.gov.uk',
       //postgres: 'https://uat-api-core.london.cloudapps.digital',
       postgres: 'https://uat.ppg-sso-service.crowncommercial.gov.uk/dashboard-wrapper',
       //cii: 'https://conclave-cii-integration-brash-shark-mk.london.cloudapps.digital',
