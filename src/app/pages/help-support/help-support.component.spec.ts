59eec8926dafbdabed3d3dae4625a13bb1114537
viniciuscerezer-bc <vinicius.cerezer@brickendon.com>
2023-10-09 03:29:45 -0400

    complete data-migration spec tests

--- src/app/pages/help-support/help-support.component.spec.ts
+++ src/app/pages/help-support/help-support.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HelpAndSupportComponent } from './help-support-component';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { of } from 'rxjs';
 
 describe('HelpAndSupportComponent', () => {
   let component: HelpAndSupportComponent;
   let fixture: ComponentFixture<HelpAndSupportComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
 
   beforeEach(async () => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['isAuthenticated']);
+
     await TestBed.configureTestingModule({
-      declarations: [ HelpAndSupportComponent ]
-    })
-    .compileComponents();
+      declarations: [HelpAndSupportComponent],
+      imports: [StoreModule.forRoot({}), BrowserAnimationsModule],
+      providers: [{ provide: AuthService, useValue: authServiceSpy }],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +30,26 @@ describe('HelpAndSupportComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show breadcrumbs when authenticated', () => {
+    authServiceSpy.isAuthenticated.and.returnValue(of(true));
+    fixture.detectChanges();
+    const breadcrumbs =
+      fixture.nativeElement.querySelector('.govuk-breadcrumbs');
+    expect(breadcrumbs).toBeTruthy();
+  });
+
+  it('should not show breadcrumbs when not authenticated', () => {
+    authServiceSpy.isAuthenticated.and.returnValue(of(false));
+    fixture.detectChanges();
+    const breadcrumbs =
+      fixture.nativeElement.querySelector('.govuk-breadcrumbs');
+    expect(breadcrumbs).toBeFalsy();
+  });
+
+  it('should display "Page not found" heading', () => {
+    const heading = fixture.nativeElement.querySelector('h1');
+    expect(heading.textContent).toContain('Page not found');
   });
 });
