6cb23ab055924b17dc3dca7b7639ae000b751078
SARANYA <SARANYA@DESKTOP-1G4R5OO>
2023-10-09 12:42:33 +0530

    TASK-6695 Angular unit testing for contact service

--- src/app/pages/contactus/contactus.component.spec.ts
+++ src/app/pages/contactus/contactus.component.spec.ts
@@ -1,17 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
 import { ContactUsComponent } from './contactus.component';
+import { ViewportScroller } from '@angular/common';
+import { PatternService } from 'src/app/shared/pattern.service';
+import { of } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('ContactUsComponent', () => {
   let component: ContactUsComponent;
   let fixture: ComponentFixture<ContactUsComponent>;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
+  
+  const mockStore = {
+    select: () => of({}),
+    dispatch: () => {},
+  };
+  const mockViewportScroller = {
+    scrollToPosition: () => {},
+  };
+  const mockPatternService = {
+    emailPattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+    emailValidator: () => true, 
+  };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ContactUsComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ContactUsComponent],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        TranslateModule.forRoot(),
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: Store, useValue: mockStore },
+        { provide: PatternService, useValue: mockPatternService },
+      ],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactUsComponent);
@@ -19,7 +50,40 @@ describe('ContactUsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize the form', () => {
+    expect(component.formGroup).toBeDefined();
+  });
+
+  it('should set submitted to true on form submission', () => {
+    component.onSubmit(component.formGroup);
+    expect(component.submitted).toBeTruthy();
+  });
+
+  it('should validate email using emailValidator', () => {
+    spyOn(mockPatternService, 'emailValidator').and.returnValue(true);
+    component.formGroup.controls['email'].setValue('test@example.com');
+    component.onSubmit(component.formGroup);
+    expect(mockPatternService.emailValidator).toBeTruthy();
+  });
+
+  it('should set email control error when email is invalid', () => {
+    spyOn(mockPatternService, 'emailValidator').and.returnValue(false);
+    component.formGroup.controls['email'].setValue('invalid-email');
+    component.onSubmit(component.formGroup);
+    expect(component.formGroup.controls['email'].hasError('incorrect')).toBeFalsy();
+  });
+
+  it('should return true for formValid when form is valid', () => {
+    component.formGroup.controls['firstName'].setValue('John');
+    component.formGroup.controls['email'].setValue('test@example.com');
+    expect(component.formValid(component.formGroup)).toBeTruthy();
+  });
+
+  it('should return false for formValid when form is invalid', () => {
+    expect(component.formValid(component.formGroup)).toBeFalsy();
   });
 });
