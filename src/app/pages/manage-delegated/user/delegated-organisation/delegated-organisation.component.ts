e2c946092bb696ed5166a677eb653bfef35a749d
SARANYA <SARANYA@DESKTOP-1G4R5OO>
2023-10-10 13:53:10 +0530

    Fixed-Bug-6726

--- src/app/pages/manage-delegated/user/delegated-organisation/delegated-organisation.component.ts
+++ src/app/pages/manage-delegated/user/delegated-organisation/delegated-organisation.component.ts
@@ -17,6 +17,7 @@ export class DelegatedOrganisationComponent implements OnInit {
   private roleData:any;
   private roleInfo: any;
   private isDeleagation:boolean=environment.appSetting.hideDelegation
+  public isOrgAdmin: boolean = false; 
 
   constructor(
     private route: Router,
@@ -24,6 +25,7 @@ export class DelegatedOrganisationComponent implements OnInit {
     private delegatedService: WrapperUserDelegatedService,
     private DelegateService: ManageDelegateService,
   ) {
+     this.isOrgAdmin = JSON.parse(localStorage.getItem('isOrgAdmin') || 'false');
     if(this.isDeleagation === true){
       this.route.navigateByUrl('/home');
       return
