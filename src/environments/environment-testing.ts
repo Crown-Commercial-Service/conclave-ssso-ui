7e471213de01306f4cf905ca9ae028e5b1666198
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2023-11-15 21:54:05 +0530

    Update environment-testing.ts

--- src/environments/environment-testing.ts
+++ src/environments/environment-testing.ts
@@ -73,7 +73,7 @@ export const environment = {
    hideAutoValidation:false,
    hideSimplifyRole:false,
    blockedScheme: ["GB-PPG"],
-   isMaintenance: true,    
+   isMaintenance: false,    
    customMfaEnabled : true,
   },
 };
